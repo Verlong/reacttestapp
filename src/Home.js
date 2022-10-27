4621918fc9350daf25d8a6d9bb3de82b8e267e8b
Verlong <bonko23@gmail.com>
2022-10-27 11:46:22 +0100

    click events

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,20 @@
 const Home = () => {
-    return ( 
-        <div className="home">
-            <h2>Homepage</h2>
-        </div>
 
-     );
-}
- 
-export default Home;
+    const handleClick = (e) => {
+      console.log('hello guest', e);
+    }
+  
+    const handleClickAgain = (name, e) => {
+      console.log('hello ' + name, e.target);
+    }
+  
+    return (
+      <div className="home">
+        <h2>Homepage</h2>
+        <button onClick={handleClick}>Click me</button>
+        <button onClick={(e) => handleClickAgain('Maryna', e)}>Click me again</button>
+      </div>
+    );
+  }
+  
+  export default Home;
