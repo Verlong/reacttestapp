63fff6c3e8d6456ec9d6aaaaf8a4f96cb6554aba
Verlong <bonko23@gmail.com>
2022-11-01 13:45:41 +0000

    new props

--- src/Home.js
+++ src/Home.js
@@ -6,11 +6,15 @@ const Home = () => {
     { title: 'My new website', body: 'lorem ipsum...', author: 'Maryna', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Kate', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Nikita', id: 3 }
-  ])
+  ]);
+  const handleDelete = (id) =>{
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
       <BlogList blogs={blogs.filter((blog)=> blog.author==='Maryna')} title="Marina's blogs" />
     </div>
   );
